2174ee472a9c1383427115c23b4aadb3eb7b2479
leekats <lee@hotmail.co.il>
2018-12-31 10:11:22 +0200

    Enable cross partitioning

--- controllers/ctrDatabase.js
+++ controllers/ctrDatabase.js
@@ -6,11 +6,14 @@ var cosmos = require('../dbconnection');
 exports.getData = (req, res) => {
     var queryString = "SELECT c.blobName, c.data, c.senderName, c._ts FROM root c ORDER BY c._ts DESC";
     const header = 'dbs/Workshop/colls/result';
-    queryExecution(header, queryString, res);
+    const options = {
+        enableCrossPartitionQuery: true
+    }
+    queryExecution(header, queryString, options, res);
 }
 
-let queryExecution = (header, queryString, res) => {
-    cosmos.client.queryDocuments(header, queryString).toArray((err, obj) => {
+let queryExecution = (header, queryString, options, res) => {
+    cosmos.client.queryDocuments(header, queryString, options).toArray((err, obj) => {
         if (err) {
             console.log(err);
             res.send([]);
